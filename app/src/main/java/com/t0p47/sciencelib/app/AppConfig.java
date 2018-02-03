e651d664d58048d140e2032176cde0cc7877916f
t0p47 <t0p47@yandex.ru>
2018-02-03 18:31:21 +0700

    FromPC(Recycle list show)

--- app/src/main/java/com/t0p47/sciencelib/app/AppConfig.java
+++ app/src/main/java/com/t0p47/sciencelib/app/AppConfig.java
@@ -6,26 +6,26 @@ package com.t0p47.sciencelib.app;
 
 public class AppConfig {
 
-    public static String URL_SYNC_FOLDERS = "http://45.76.186.7/api/android/sync/folders";
+    public static String URL_SYNC_FOLDERS = "http://45.76.158.9/api/android/sync/folders";
 
-    public static String URL_SYNC_ARTICLES = "http://45.76.186.7/api/android/sync/articles";
+    public static String URL_SYNC_ARTICLES = "http://45.76.158.9/api/android/sync/articles";
 
-    public static String URL_LOGIN_USER = "http://45.76.186.7/api/authenticate";
+    public static String URL_LOGIN_USER = "http://45.76.158.9/api/authenticate";
 
-    public static String URL_REGISTER_USER = "http://45.76.186.7/api/register";
+    public static String URL_REGISTER_USER = "http://45.76.158.9/api/register";
 
-    public static String URL_REFRESH_TOKEN = "http://45.76.186.7/api/refresh-token";
+    public static String URL_REFRESH_TOKEN = "http://45.76.158.9/api/refresh-token";
 
-    //TODO: Две следующих ссылки должны быть "http://45.76.186.7/api/android/sync/articles"; "http://45.76.186.7/api/android/sync/folders";
+    //TODO: Две следующих ссылки должны быть "http://45.76.158.9/api/android/sync/articles"; "http://45.76.186.7/api/android/sync/folders";
     //А сейчас просто временно
 
-    public static String URL_SEND_FOLDERS = "http://45.76.186.7/api/android/send/folders";
+    public static String URL_SEND_FOLDERS = "http://45.76.158.9/api/android/send/folders";
 
-    public static String URL_SEND_ARTICLES = "http://45.76.186.7/api/android/send/articles";
+    public static String URL_SEND_ARTICLES = "http://45.76.158.9/api/android/send/articles";
 
-    public static String URL_FOLDER_REQUEST_BACK = "http://45.76.186.7/api/android/folder/request";
+    public static String URL_FOLDER_REQUEST_BACK = "http://45.76.158.9/api/android/folder/request";
 
-    public static String URL_ARTICLE_REQUEST_BACK = "http://45.76.186.7/api/android/article/request";
+    public static String URL_ARTICLE_REQUEST_BACK = "http://45.76.158.9/api/android/article/request";
 
 
 
